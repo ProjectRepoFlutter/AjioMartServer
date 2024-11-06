e8e58ec09b1ad59f96faebb8194fd455dd6af204
Ajay Raj <ajayrajsaini333@gmail.com>
2024-11-06 19:52:15 +0530

    addded fast2sms

--- utils/notification.js
+++ utils/notification.js
@@ -1,5 +1,5 @@
 const nodemailer = require('nodemailer');
-const twilio = require('twilio'); // Use Twilio for SMS
+const axios = require('axios');
 
 // Email setup (using nodemailer)
 const transporter = nodemailer.createTransport({
@@ -31,15 +31,24 @@ exports.sendVerificationCode = async (identifier, verificationCode) => {
         });
         console.log("mail sent");
     } else { // If it's a phone number
-        console.log("using phone");
-        const accountSid = 'your-twilio-account-sid';
-        const authToken = 'your-twilio-auth-token';
-        const client = twilio(accountSid, authToken);
-
-        await client.messages.create({
-            body: `Your verification code is: ${verificationCode}`,
-            from: 'your-twilio-phone-number',
-            to: identifier,
-        });
+        try {
+            const response = await axios.post('https://www.fast2sms.com/dev/bulkV2', {
+                route: "dlt",
+                sender_id: "GYTRGP",
+                message: "175236", // Replace this with your actual template ID
+                variables_values: `${verificationCode}|`, // Sending verification code here
+                flash: 0,
+                numbers: identifier // Phone number to send the SMS
+            }, {
+                headers: {
+                    "authorization": "uyZG4YRXnhAvSU5KDCQbm2gJIpTeLOcrad3j0tzMW86kflwPE74TrQ5AWOe09XGR7yH6YUupKNjMv3Jo", // Your Fast2SMS API Key
+                    "Content-Type": "application/json"
+                }
+            });
+    
+            console.log("SMS sent successfully:", response.data);
+        } catch (error) {
+            console.error("Error sending SMS:", error);
+        }
     }
 };
